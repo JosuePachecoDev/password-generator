ec8a0ea126051cb0baabad34505ad7f1aa22e346
Josué Benjamín Pacheco <josue2.benjamin6@gmail.com>
2026-05-14 15:27:15 -0600

    fixed: files structur v1

--- src/js/main.js
+++ src/js/main.js
@@ -12,12 +12,6 @@ const checkboxes = document.querySelectorAll(".chkb");
 const copyButton = document.getElementById("copy-button");
 
 const secChar = document.getElementById("char");
-secChar.style.border = "solid 1px #000";
-secChar.style.borderRadius = "8px";
-secChar.style.marginTop = "24px";
-secChar.style.width = "100%";
-secChar.style.padding = "0 16px 16px 16px";
-secChar.style.boxSizing = "border-box";
 
 const insertElement = (element, txtCon, father) => {
     element.innerHTML = txtCon;
